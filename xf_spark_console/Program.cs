6042bd1be4923017e3b27a7e9af5ff3550c31347
kensen <kensen04@163.com>
2024-04-15 20:27:42 +0800

    up

--- xf_spark_console/Program.cs
+++ xf_spark_console/Program.cs
@@ -6,7 +6,7 @@ using xf_spark_console.SparkDesk;
 var t=ConfigRead.GetApiConfigDto();
 
 Console.WriteLine($"Hello, World!欢迎使用讯飞火星大模型");
-Console.WriteLine("请选择一个大模型版本：");
+Console.WriteLine("请选择一个大模型版本： ");
 
 var index = 1;
 foreach (var m in t.ModelUrls)
