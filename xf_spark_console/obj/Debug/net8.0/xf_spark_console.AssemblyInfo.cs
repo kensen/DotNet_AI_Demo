e795facce194fc2b6ac9f109b5419738a58187b7
kensen <kensen04@163.com>
2024-11-18 20:06:20 +0800

    up

--- xf_spark_console/obj/Debug/net8.0/xf_spark_console.AssemblyInfo.cs
+++ xf_spark_console/obj/Debug/net8.0/xf_spark_console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xf_spark_console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44391556362c868d61d286082a425e9a3ce7c8da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3813f70744f33fb408b422d3a644933b67d821a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("xf_spark_console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xf_spark_console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
