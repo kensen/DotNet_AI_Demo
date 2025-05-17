05924bd4f2f7764d405a0d9ea42c0ec497b180e7
kensen <kensen04@163.com>
2025-05-17 20:24:11 +0800

    up

---
+++ SemanticKernel_Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//提升用户输入，之后打印用户输入的内容
+Console.WriteLine("请输入您的名字：");
+string userInput = Console.ReadLine();
+Console.WriteLine("您输入的内容是：");
+Console.WriteLine(userInput);
+Console.WriteLine("请输入您的年龄：");
+string userAge = Console.ReadLine();
+Console.WriteLine("您输入的年龄是：");
+Console.WriteLine(userAge);
+//等待用户输入
--- SemanticKernel_Demo/obj/Debug/net9.0/SemanticKernel_Demo.AssemblyInfo.cs
+++ SemanticKernel_Demo/obj/Debug/net9.0/SemanticKernel_Demo.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("xf_spark_console")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SemanticKernel_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d579628236edeadb48ac18fd7b6deb510dc8d50")]
-[assembly: System.Reflection.AssemblyProductAttribute("xf_spark_console")]
-[assembly: System.Reflection.AssemblyTitleAttribute("xf_spark_console")]
+[assembly: System.Reflection.AssemblyProductAttribute("SemanticKernel_Demo")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SemanticKernel_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
